f87e0586dbbf4f8763efb0a4548f41e1fc82d7dd
Milia <milia.menzatova@gmail.com>
2026-01-13 10:20:03 -0500

    OPP

---
+++ first-project/src/main/java/com/oops/Student.java
@@ -0,0 +1,25 @@
+package com.oops;
+
+public class Student {
+    //variables
+    int rollnum;
+    String name;
+    char grade;
+    String address;
+
+    //methods
+    void displayAllValues(){
+        System.out.println(rollnum);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(address);
+    }
+
+
+
+    public static void main(String[] args) {
+
+           }
+
+    }
+
