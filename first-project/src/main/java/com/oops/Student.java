e40a69acbf531b17ca570ebedbd943e4dba6fa70
Milia <milia.menzatova@gmail.com>
2026-01-14 12:25:32 -0500

    test

--- first-project/src/main/java/com/oops/Student.java
+++ first-project/src/main/java/com/oops/Student.java
@@ -14,10 +14,25 @@ public class Student {
         System.out.println(grade);
         System.out.println(address);
     }
+    void displayRollNum(){
+        System.out.println("RollNum value is : " + rollnum);
+    }
 
 
 
     public static void main(String[] args) {
+        // creating th object
+        Student stu1 = new Student();
+        //assigned some values to the variables in the run time
+        stu1.rollnum = 1;
+        stu1.name = "Milya";
+        stu1.grade = 'A';
+        stu1.address = "Canada";
+
+        //call methods
+        stu1.displayAllValues();
+        stu1.displayRollNum();
+
 
            }
 
