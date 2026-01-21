f6c2044c5a460cd34b44d608d5aa2462f1f4b409
Milia <milia.menzatova@gmail.com>
2026-01-20 22:34:55 -0500

    Exm

---
+++ first-project/src/main/java/com/keyword/Test1.java
@@ -0,0 +1,11 @@
+package com.keyword;
+
+public class Test1 {
+  static String str = "Milya";
+
+
+    public static void main(String[] args) {
+        //String s = Test1.str;
+        System.out.println(Test1.str.length());
+    }
+}
