a702641019ce833d7b99e996b21a2845864526cc
Milia <milia.menzatova@gmail.com>
2026-02-06 16:30:45 -0500

    add

---
+++ first-project/src/main/java/com/abstraction/Overview.java
@@ -0,0 +1,9 @@
+package com.abstraction;
+
+// Abstraction is a process of hiding the implementation details
+// and showing only the functionality to the user
+//1. Abstract class - Partial Abstraction (100% or less)
+//2. Interface - 100% - Pure Abstraction
+
+public class Overview {
+}
--- first-project/src/main/java/com/keyword/SuperKeyword.java
+++ first-project/src/main/java/com/keyword/SuperKeyword.java
@@ -4,6 +4,9 @@ import java.sql.SQLOutput;
 
 class Car{
     int speed = 100;
+    void accelerate(){
+        System.out.println("Accelerate method in the Parent class");
+    }
 }
 
 class BMW extends Car{
@@ -12,12 +15,21 @@ class BMW extends Car{
         System.out.println("Test" + speed);
         System.out.println(super.speed);
     }
+    void accelerate(){
+        //System.out.println("Accelerate method in the Child class");
+        super.accelerate();
+        //Immediate parent class method we cannot use in main, only in the child clas
+        // to call parent class
+    }
 
 }
 public class SuperKeyword {
     public static void main(String[] args) {
         BMW car = new BMW();
         car.displaySpeed();
+
+        BMW obj = new BMW();
+        obj.accelerate();
 
         System.out.println(car);
     }
