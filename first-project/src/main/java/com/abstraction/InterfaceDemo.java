894838c97f68256c21b25af3b0b2d82d36f25e40
Milia <milia.menzatova@gmail.com>
2026-02-11 11:50:42 -0500

    add

--- first-project/src/main/java/com/abstraction/InterfaceDemo.java
+++ first-project/src/main/java/com/abstraction/InterfaceDemo.java
@@ -6,15 +6,32 @@ interface Book {
 
     void method1();  //abstract method without body
     default void method2(){
-        System.out.println("we need use key word default");
-    }
+        System.out.println("Default method");
+    } //we need use key word default
    static void method3(){
-        System.out.println("we need yo specify static or default");
+        System.out.println("Static method");
     }
 
 }
-public class InterfaceDemo {
+public class InterfaceDemo implements Book{
+
+    public void method1(){
+        System.out.println("Abstract method code");
+    }
     public static void main(String[] args) {
+      /*InterfaceDemo obj = new InterfaceDemo();
+      obj.method1();
+      obj.method2();
+      //obj.method3();//we cannot write like this
+      Book.method3(); //Using Interface we can call static method
+
+       */
+        //Case 2. Using Interface reference variable to call the child class
+        //Book bk = new Book(); //not possible to create obj for interface
+        Book bk = new InterfaceDemo(); //to use child class
+        bk.method1();
+        bk.method2();
+        Book.method3();
 
     }
 }
