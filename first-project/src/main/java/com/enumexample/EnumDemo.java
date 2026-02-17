a980f8286d1548451fd4715546a7b64e1e3c1303
Milia <milia.menzatova@gmail.com>
2026-02-17 12:23:40 -0500

    add

---
+++ first-project/src/main/java/com/enumexample/EnumDemo.java
@@ -0,0 +1,10 @@
+package com.enumexample;
+
+public class EnumDemo {
+    //Enum - Enumeration in Java
+    // Enum is a special class in Java that allows to defined predefined constants
+    //Enum constants are public static final
+    public static void main(String[] args) {
+        System.out.println(Week.MONDAY);
+    }
+}
---
+++ first-project/src/main/java/com/enumexample/Week.java
@@ -0,0 +1,11 @@
+package com.enumexample;
+
+public enum Week { //ENUM to used to define constant
+   SUNDAY,
+   MONDAY,
+   TUESDAY,
+   WEDNESDAY,
+   THURSDAY,
+   FRIDAY,
+   SATURDAY
+}
