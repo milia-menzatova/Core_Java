d743ee6380877b715db686fbc094b711ddfde69f
Milia <milia.menzatova@gmail.com>
2026-02-26 11:49:01 -0500

    add

--- first-project/src/main/java/com/exception/ExceptionExample.java
+++ first-project/src/main/java/com/exception/ExceptionExample.java
@@ -6,6 +6,7 @@ public class ExceptionExample {
     public static void main(String[] args) {
 
         System.out.println("Start of the program");
+        Scanner sc = new Scanner(System.in);
        /*
         //Example1
         Scanner sc = new Scanner(System.in);
@@ -16,8 +17,33 @@ public class ExceptionExample {
 
         */
 
-        //ExampleЛ
+        //Example2
+       /* int arr[] = new int[5];
+        System.out.println("Enter an position(0-4): ");
+        int position = sc.nextInt();
+
+        System.out.println("Enter an value:");
+        int value = sc.nextInt();
+
+        arr[position] = value;
+        System.out.println(arr[position]);
+        System.out.println(value);
+        */
+
+        //Example3
+       /*// String str = "12345";
+        String str = "Milya";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+
+        */
+        //Example4
+        //String s = "Milya";
+       // String s = "";
+        String s = null;
+        System.out.println(s.length());
 
         System.out.println("End of the program");
+
     }
 }
