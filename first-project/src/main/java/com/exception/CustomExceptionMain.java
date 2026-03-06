e11a13694a8812f2d7bf4be0b628accfe33087ad
Milia <milia.menzatova@gmail.com>
2026-03-06 15:07:35 -0500

    add

---
+++ first-project/src/main/java/com/exception/CustomExceptionMain.java
@@ -0,0 +1,7 @@
+package com.exception;
+
+public class CustomExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception("Milya");
+    }
+}
---
+++ first-project/src/main/java/com/exception/TestException.java
@@ -0,0 +1,9 @@
+package com.exception;
+
+public class TestException extends Exception{
+    //Custom Exception is user defined Exception
+
+    TestException(String message){
+        super(message);
+    }
+}
