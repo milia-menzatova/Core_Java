54fd4958fa763f12704a702e35ea5bf8a2f3c11c
Milia <milia.menzatova@gmail.com>
2026-03-26 16:51:54 -0400

    array, set, add, insert

--- first-project/src/main/java/com/collection/ArrayListDemo.java
+++ first-project/src/main/java/com/collection/ArrayListDemo.java
@@ -14,10 +14,25 @@ public class ArrayListDemo {
         arrList.add(100);
         arrList.add("Milya");
         arrList.add(true);
+        arrList.add(234.567);
+        arrList.add(null);
+        arrList.add(100);
+
 
         //Size of the ArrayList
         int size = arrList.size();
         System.out.println(size);
+
+        //Print the values in the array list
+        System.out.println(arrList);
+
+        //Removing the elements - ArrayList
+        arrList.remove(5);
+        System.out.println(arrList);
+
+        //Insert element at specific index
+        arrList.add(3, "Test");
+        System.out.println(arrList);
 
 
     }
