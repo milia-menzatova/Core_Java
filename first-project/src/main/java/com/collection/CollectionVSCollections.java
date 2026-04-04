d8e7392f2e6343b24617e68635cdd749b74fc391
Milia <milia.menzatova@gmail.com>
2026-04-04 15:09:42 -0400

    collections vs collection

---
+++ first-project/src/main/java/com/collection/CollectionVSCollections.java
@@ -0,0 +1,19 @@
+package com.collection;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CollectionVSCollections {
+    //Collection is an Interface in Java
+    //Group of Individual objects
+
+    //Collections is a Class in Java - Utility class
+    //static methods - sort, binary search, swap, emptyList
+
+
+    public static void main(String[] args) {
+        Collection<Integer>c;
+        Collections.sort(null);
+
+    }
+}
