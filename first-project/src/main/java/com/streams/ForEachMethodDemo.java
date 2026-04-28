85c0f43631b044d259547a50b47812042fd56901
Milia <milia.menzatova@gmail.com>
2026-04-28 16:58:01 -0400

    forEach

---
+++ first-project/src/main/java/com/streams/ForEachMethodDemo.java
@@ -0,0 +1,27 @@
+package com.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ForEachMethodDemo {
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(23, 32, 11, 43, 88);
+        arr.forEach(n -> System.out.println(n));
+
+       /* //Normal for loop
+        for(int i=0; i<arr.size(); i++){
+            System.out.println(arr.get(i) );
+        }
+
+        */
+
+        /*//Enhanced for loop - foreach
+        for(int i:arr){
+            System.out.println(i);
+        }
+         */
+
+    }
+
+
+}
