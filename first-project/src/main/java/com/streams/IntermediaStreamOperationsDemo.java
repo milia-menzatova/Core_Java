9523047074877420a97bcffec1e90d948e233eb3
Milia <milia.menzatova@gmail.com>
2026-04-30 16:48:14 -0400

    Stream - distinct method

--- first-project/src/main/java/com/streams/IntermediaStreamOperationsDemo.java
+++ first-project/src/main/java/com/streams/IntermediaStreamOperationsDemo.java
@@ -10,12 +10,18 @@ public class IntermediaStreamOperationsDemo {
         //Filter() - based on condition - keep only matching elements
         List<Integer> num1 = Arrays.asList(10, 20, 30, 40, 50);
         Stream<Integer> st1 = num1.stream().filter(n-> n%20==0);
-        st1.forEach(n-> System.out.println(n));
+       // st1.forEach(n-> System.out.println(n));
 
 
-        //Map(Function) - Transform each element
+        //Map(Function) - Transform each element, transform to another stream
         List<String> names = Arrays.asList("Milya", "Test", "Tom");
         Stream<Integer> namesLength = names.stream().map(n->n.length());
-        namesLength.forEach(n-> System.out.println(n));
+       // namesLength.forEach(n-> System.out.println(n));
+
+        //Distinct() - remove duplicates
+        List<Integer> num2 = Arrays.asList(1,1,1,1,1,5,7,7, 88);
+        Stream<Integer>str3 = num2.stream().distinct();
+        str3.forEach(n-> System.out.println(n));
+
     }
 }
