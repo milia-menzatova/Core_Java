95f221742c6109a9b1ef5aa0ad34b63a7020c4f4
Milia <milia.menzatova@gmail.com>
2026-04-30 16:40:07 -0400

    Stream - map method

--- first-project/src/main/java/com/streams/IntermediaStreamOperationsDemo.java
+++ first-project/src/main/java/com/streams/IntermediaStreamOperationsDemo.java
@@ -7,9 +7,15 @@ import java.util.stream.Stream;
 public class IntermediaStreamOperationsDemo {
     public static void main(String[] args) {
 
-        //Filter() - based on condition
+        //Filter() - based on condition - keep only matching elements
         List<Integer> num1 = Arrays.asList(10, 20, 30, 40, 50);
         Stream<Integer> st1 = num1.stream().filter(n-> n%20==0);
         st1.forEach(n-> System.out.println(n));
+
+
+        //Map(Function) - Transform each element
+        List<String> names = Arrays.asList("Milya", "Test", "Tom");
+        Stream<Integer> namesLength = names.stream().map(n->n.length());
+        namesLength.forEach(n-> System.out.println(n));
     }
 }
